0caa03d01227f3e02601369b06be451e01cf658b
Dominika Barrett <barrettdomi@gmail.com>
2020-09-22 11:55:06 +0100

    add file

--- reports.py
+++ reports.py
@@ -10,18 +10,28 @@ def all_games_data(file_name):
     return result
 
 
+def alfa_sort(to_sort):
+    x = len(to_sort)
+    for i in range(x):
+        for j in range(0,x-i-1):
+            if to_sort[j] > to_sort[j+1]:
+                to_sort[j] ,to_sort[j+1]  = to_sort[j +1],to_sort[j]
+    return to_sort
+
+
+
+
 def count_games(file_name):
-    with open(file_name, 'r') as line:
-        i = 0
-        for _ in line:
-            print(type(_))
-            # print(_)
-            i += 1
-        return line.readlines()
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    i = 0
+    for _ in lines:
+        i += 1
+    return i
 
 
-#
-# print(count_games('game_stat.txt'))
+print(count_games('game_stat.txt'))
+
 
 def decide(file_name, year):
     lines = all_games_data(file_name)
@@ -72,8 +82,13 @@ print(get_line_number_by_title('game_stat.txt', 'Populous'))
 
 
 def sort_abc(file_name):
-    pass
+    lines = all_games_data(file_name)
+    titels = [i[0] for i in lines]
+    return alfa_sort(titels)
 
+
+
+print(sort_abc("game_stat.txt"))
 
 def get_genres(file_name):
     lines = all_games_data(file_name)
@@ -81,7 +96,8 @@ def get_genres(file_name):
     for i in lines:
         if not i[3] in genre:
             genre.append(i[3])
-    return genre
+    return alfa_sort(genre)
+
 
 
 # TODO sortowanie wlasnej roboty
@@ -93,9 +109,11 @@ def when_was_top_sold_fps(file_name):
     lines = all_games_data(file_name)
     top_sold = [0, 0, 0]
     for i in lines:
-        if (i[3] == 'First-person shooter') and (i[1] > top_sold[2]): #zdanie logiczne
+        if (i[3] == 'First-person shooter') and (i[1] > top_sold[2]):  # zdanie logiczne
             top_sold = i
     if top_sold[2] == 0:
         raise ValueError
     return top_sold[2]
+
+
 print(when_was_top_sold_fps('game_stat.txt'))
